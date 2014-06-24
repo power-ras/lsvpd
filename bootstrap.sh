0abe246482dcffee2054df58a13c7ac2542120fb
Vasant Hegde <hegdevasant@linux.vnet.ibm.com>
2014-06-24 14:30:42 +0530

    lsvpd: spec file cleanup
    This patch does below cleanup:
     - Removed librtas checking as we need librtas for building lsvpd
     - Remove white space
     - Remove BuildRoot (we are using RPM_BUILD_ROOT)
     - Updated BuildRequires and post Requires details
     - Updated Source link
     - Removed INSTALL and NEWS file
    
    Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>
    Acked-by: Suzuki K. Poulose <suzuki@in.ibm.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 set -x
 aclocal && libtoolize --force && autoheader && automake --add-missing --copy && autoconf && \
-chmod 644 lsvpd.spec.in Makefile.am NEWS README scsi_templates.conf \
+chmod 644 lsvpd.spec.in Makefile.am README scsi_templates.conf \
 INSTALL COPYING configure.ac ChangeLog cpu_mod_conv.conf
