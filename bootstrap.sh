3e8042b2f718ae9914e52889d5d4ebc30ec04d9b
Vasant Hegde <hegdevasant@linux.vnet.ibm.com>
2014-07-01 15:50:40 +0530

    Add AC_PREREQ to configure.ac
    ..courtesy autoscan. Also,
      - Added more macro's to configure.ac file
      - Created config and m4 directories.
      - Removed unused NEWS file
    
    Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>
    Acked-by: Suzuki K. Poulose <suzuki@in.ibm.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,15 @@
 set -x
+
+if [ ! -d config ];
+then
+	mkdir config;
+fi
+
+if [ ! -d m4 ];
+then
+	mkdir m4;
+fi
+
 aclocal && libtoolize --force && autoheader && automake --add-missing --copy && autoconf && \
 chmod 644 lsvpd.spec.in Makefile.am README scsi_templates.conf \
 INSTALL COPYING configure.ac ChangeLog cpu_mod_conv.conf
