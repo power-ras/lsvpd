d42e0d68ce2ea3c63d533ae12c0d23b05980a981
Janani Venkataraman <jananive@linux.vnet.ibm.com>
2014-10-27 18:12:29 +0530

    lsmcode: fix path issue with -z option
    Fixing the path issue in lsmcode -z option when using a db file
    to retrieve the vpd information. Also removing the message "Run
    vpdupdate" when we try to retrieve the vpd  information from a
    db archive file. Providing  "Run vpdupdate" message to user is
    not relevant in this context as we have already mentioned the file.
    
    Signed-off-by: Janani Venkataraman <jananive@linux.vnet.ibm.com>
    Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>

--- src/output/lsmcode.cpp
+++ src/output/lsmcode.cpp
@@ -440,7 +440,11 @@ int main( int argc, char** argv )
 		}
 
 		index = path.rfind( "/" );
-		env = path.substr( 0, index + 1 );
+		if(index == -1)
+			env = "./";
+		else
+			env = path.substr( 0, index + 1 );
+
 		db = path.substr( index + 1 );
 		try
 		{
@@ -448,13 +452,7 @@ int main( int argc, char** argv )
 		}
 		catch( exception& e )
 		{
-			string prefix( DEST_DIR );
-			cout << "Please run " << prefix;
-			if( prefix[ prefix.length( ) - 1 ] != '/' )
-			{
-				cout << "/";
-			}
-			cout << "sbin/vpdupdate before running lsmcode." << endl;
+			cout << "Unable to process vpd DB " << path << ". Possibly corrupted DB" << endl;
 			return 1;
 		}
 	}
