771285c434a7cce4347c0d74cff0e6064d393ebf
Vasant Hegde <hegdevasant@linux.vnet.ibm.com>
2015-07-20 17:10:35 +0530

    lsvpd: Silence automake warning
    Latest automake throws below warning. Silence this warning by updating configure.ac.
    Also remove INSTALL file.. as it is just a copy of INSTALL file from autotools.
    I don't think we have to ship that file.
    
    Sample log without this path:
    -----------------------------
    Makefile.am:41: warning: source file 'src/output/lscfg.cpp' is in a subdirectory,
    Makefile.am:41: but option 'subdir-objects' is disabled
    automake: warning: possible forward-incompatibility.
    automake: At least a source file is in a subdirectory, but the 'subdir-objects'
    automake: automake option hasn't been enabled.  For now, the corresponding output
    automake: object file(s) will be placed in the top-level directory.  However,
    automake: this behaviour will change in future Automake versions: they will
    automake: unconditionally cause object files to be placed in the same subdirectory
    automake: of the corresponding sources.
    automake: You are advised to start using 'subdir-objects' option throughout your
    automake: project, to avoid future incompatibilities.
    
    Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,4 +12,4 @@ fi
 
 aclocal && libtoolize --force && autoheader && automake --add-missing --copy && autoconf && \
 chmod 644 lsvpd.spec.in Makefile.am README scsi_templates.conf \
-INSTALL COPYING configure.ac ChangeLog cpu_mod_conv.conf
+COPYING configure.ac ChangeLog cpu_mod_conv.conf
